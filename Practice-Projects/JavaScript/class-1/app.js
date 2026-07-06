13acd31d1a8198f27aabf26fe7495ce0feac227e
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-06 15:48:12 +0530

    1st question

---
+++ Practice-Projects/JavaScript/class-1/app.js
@@ -0,0 +1,7 @@
+// Even OR Odd Number //
+
+let number1 = Number(prompt("Enter Your Number"));
+
+let result = number1 % 2 === 0 ? "Your Number is Even" : "Your Number is Odd";
+
+console.log(result);
