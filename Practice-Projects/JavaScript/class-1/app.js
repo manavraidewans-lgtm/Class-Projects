ee7673ba2f87a6f6f9e64263426740fbed001ecb
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-06 16:16:32 +0530

    2 question done

--- Practice-Projects/JavaScript/class-1/app.js
+++ Practice-Projects/JavaScript/class-1/app.js
@@ -1,7 +1,29 @@
-// Even OR Odd Number //
 
-let number1 = Number(prompt("Enter Your Number"));
 
-let result = number1 % 2 === 0 ? "Your Number is Even" : "Your Number is Odd";
+// // Even OR Odd Number //
 
-console.log(result);
+// let number1 = Number(prompt("Enter Your Number"));
+
+// let result = number1 % 2 === 0 ? "Your Number is Even" : "Your Number is Odd";
+
+// console.log(result);
+
+
+
+
+// // Buzz Number //
+
+// // 1 //
+
+// let buzNum = Number(prompt("Enter Number you wanna check")) ;
+
+// if ((buzNum % 10 == 7) || (buzNum %7 == 0) ) {
+//     console.log("Your Number is buzz") ;
+// } else {
+//     console.log("Your Number is not buzz") ;
+// }
+
+
+// //  2 //
+// let buzNum1 =  buzNum %10 == 7 || buzNum %7 == 0 ? "Your Number is buzz" : "Your number is not buzz" ;
+// console.log(buzNum1);
