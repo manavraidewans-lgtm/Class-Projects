29dafc71d4796590c16178785df674b11bb69d4d
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-07 11:00:03 +0530

    electricity bill

--- Practice-Projects/JavaScript/class-1/app.js
+++ Practice-Projects/JavaScript/class-1/app.js
@@ -27,3 +27,24 @@
 // //  2 //
 // let buzNum1 =  buzNum %10 == 7 || buzNum %7 == 0 ? "Your Number is buzz" : "Your number is not buzz" ;
 // console.log(buzNum1);
+
+
+
+
+
+// // Electricity Bill //
+
+// let units = parseInt(prompt("Enter electricity Units"));
+// let bill = 0 ;
+
+// if (units <= 0) {
+//     bill = units * 10 ;
+// } else if (units <= 200 ) {
+//     bill = (100 * 10 ) + ((units - 100) * 15) ;
+// } else if (units <= 300 ) {
+//     bill = (100 * 10 ) + ( 100 * 15 ) + ((units - 200) * 20) ;
+// } else {
+//     bill = (100 * 10 ) + (100 * 15 ) + (100 * 20 ) + ((units - 300) * 25) ;
+// }
+
+// console.log ( bill ) ;
