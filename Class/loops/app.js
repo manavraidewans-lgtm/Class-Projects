ea9971ec6408fa39382a85a8c341e3248957a610
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-12 21:17:37 +0530

    for loop done

--- Class/loops/app.js
+++ Class/loops/app.js
@@ -2,8 +2,57 @@
 
 // // Print 1 - 100 //
 
-// for ( i=1 ; i <=100 ; i++  ) {
+// for ( let i=1 ; i <=100 ; i++  ) {
 //     console.log(i);
 // }
+
+
+
+
+
+
+// // Print ODD numbers //
+
+// for (let i=1 ; i<=15 ; i++) {
+//     if ( i%3==0 ) {
+//         console.log(i)
+//     }
+// } 
+
+
+
+
+
+// console.log("END");
+
+
+
+
+
+// // Print EVEN numbers //
+
+// for (let i=1 ; i<=15 ; i++) {
+//     if ( i%2==0 ) {
+//         console.log(i)
+//     }
+// } 
+
+
+
+
+
+// console.log("END");
+
+
+
+
+
+
+// // Multiple of 5 //
+
+// for (let i=5 ; i<=100 ; i = i+5)  {
+//     console.log(i);
+// }
+
 
 
