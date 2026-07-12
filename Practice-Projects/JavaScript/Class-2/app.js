44709121beb2d5c7cd7dbc961aad86b6c719fb11
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-12 11:58:21 +0530

    buzz num added

--- Practice-Projects/JavaScript/Class-2/app.js
+++ Practice-Projects/JavaScript/Class-2/app.js
@@ -1,20 +1,28 @@
 
 
 
-// ODD EVEN //
+// // ODD EVEN //
 
-let check_odd_even = parseInt(prompt("Enter Number You wanna check")) ;
+// let check_odd_even = parseInt(prompt("Enter Number You wanna check")) ;
 
-if (check_odd_even %2 == 0) {
-    console.log("Your Num is EVEN");
-} else {
-    console.log("Your Num is ODD");
-} 
-
-
+// if (check_odd_even %2 == 0) {
+//     console.log("Your Num is EVEN");
+// } else {
+//     console.log("Your Num is ODD");
+// } 
 
 
 
 
+
+// // BUZZ NUMBER //
+
+// let buzznum = parseInt(prompt("Enter Num You wanna Check")) ;
+
+// if (buzznum %7 == 0 || buzznum %10 == 7) {
+//     console.log("Num is BUZZ");
+// } else {
+//     console.log("Num is not BUZZ");
+// }
 
 
