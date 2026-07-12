d339ce11e84d02b88fc20571edc6b0ceaa312872
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-12 11:49:04 +0530

    done odd even

---
+++ Practice-Projects/JavaScript/Class-2/app.js
@@ -0,0 +1,20 @@
+
+
+
+// ODD EVEN //
+
+let check_odd_even = parseInt(prompt("Enter Number You wanna check")) ;
+
+if (check_odd_even %2 == 0) {
+    console.log("Your Num is EVEN");
+} else {
+    console.log("Your Num is ODD");
+} 
+
+
+
+
+
+
+
+
