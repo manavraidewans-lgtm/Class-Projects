35095c50a2ead66500cef1217d65f43fb02dd0f4
Manav Rai Dewan <manavraidewans@Gmail.com>
2026-07-18 09:53:40 +0530

    random done

--- Practice-Projects/Clac/logic.js
+++ Practice-Projects/Clac/logic.js
@@ -7,7 +7,7 @@ function changeColor() {
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
 
-    if (!q) return;
+    
     q.style.backgroundColor = `rgba(${r}, ${g}, ${b})`;
 }
 
